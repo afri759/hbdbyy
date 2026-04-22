e0daa99e9aa60e3946c8a19062b8301acd6e6e40
afri759 <55058889+afri759@users.noreply.github.com>
2026-04-23 00:09:31 +0700

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+// login page scripts
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  validateForm();
+});
+
+function validateForm() {
+  var password = document.getElementById("password").value;
+
+  //validasi username dan password, username dan password bisa diganti sesuai keinginan
+  if (password === "23april2002") {
+    Swal.fire({
+      icon: "success",
+      title: "Login berhasil!",
+      text: "Selamat datang ayanggg",
+      showConfirmButton: false,
+      timer: 1500,
+    }).then(function () {
+      window.location.href = "birthday.html";
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Login gagal!",
+      text: "Coba cek lagi passwordnya ya:)",
+      confirmButtonText: "Coba lagi",
+      confirmButtonColor: "#ff7675",
+    });
+  }
+}
